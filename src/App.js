40717cceafe8ed7a3e701d5a37454a9c3ac15042
Brian-Santeliz <52502435+Brian-Santeliz@users.noreply.github.com>
2020-08-01 14:09:02 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3>Hola Mundo desde CodeSanbox!</h3>
+    </div>
+  );
+}
